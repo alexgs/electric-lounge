375b44dc5ef6e850ab0c7a76f6f0f029998574fe
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-02-27 10:56:20 -0500

    Convert auth API file to Typescript

--- webapp/pages/api/auth/[...nextauth].ts
+++ webapp/pages/api/auth/[...nextauth].ts
@@ -4,10 +4,6 @@ import * as env from 'env-var';
 import NextAuth from 'next-auth';
 import Providers from 'next-auth/providers';
 
-// NB: (2021-02-27) TypeScript support for NextAuth is apparently shit. Check
-// the [TypeScript Support][1] page for future developments.
-// [1]: https://next-auth.js.org/getting-started/typescript
-
 export default NextAuth({
   providers: [
     Providers.Spotify({
--- webapp/types/next-auth.ts
+++ webapp/types/next-auth.ts
@@ -1,3 +1,28 @@
+// NB: (2021-02-27) TypeScript support for NextAuth is apparently shit, so I'm
+// writing my own declaration in this file. Check the [TypeScript Support][1]
+// page for future developments.
+// [1]: https://next-auth.js.org/getting-started/typescript
+
+declare module 'next-auth' {
+  import { ProviderObject } from 'next-auth/providers';
+
+  function NextAuth(options: { providers: ProviderObject[] }): unknown
+
+  export default NextAuth;
+}
+
+declare module 'next-auth/providers' {
+  export type ProviderObject = unknown;
+
+  interface ProvidersList {
+    Spotify: (options: { clientId: string, clientSecret: string }) => ProviderObject;
+  }
+
+  const Providers: ProvidersList;
+
+  export default Providers;
+}
+
 declare module 'next-auth/client' {
   const Provider: React.FC<ProviderProps>;
 
@@ -17,10 +42,9 @@ declare module 'next-auth/client' {
     image: string;
   }
 
-  function signIn(): Promise<void>;
+  function signIn(): Promise<void>
 
-  function signOut(): Promise<void>;
+  function signOut(): Promise<void>
 
-  function useSession(): [Session | null, false | undefined, true];
+  function useSession(): [Session | null, false | undefined, true]
 }
-
