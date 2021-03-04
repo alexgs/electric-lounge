1f989d71957e3a81b194044d902012fa801f9457
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-03-04 08:49:09 -0500

    Make Spotify access token available in the session data

--- webapp/components/PartyWizard/SpotifyApiCheck.tsx
+++ webapp/components/PartyWizard/SpotifyApiCheck.tsx
@@ -3,6 +3,39 @@
  * the Open Software License version 3.0.
  */
 
+/* eslint-disable no-inner-declarations */
+
+import { useSession } from 'next-auth/client';
 import * as React from 'react';
 
-export const SpotifyApiCheck: React.FC = () => <div>Placeholder</div>;
+export const SpotifyApiCheck: React.FC = () => {
+  const [someState, setSomeState] = React.useState<unknown>(null);
+  const [ session, loading ] = useSession();
+
+  React.useEffect(() => {
+    if (session && !someState) {
+      async function worker() {
+        // Typescript is dumb sometimes :eyeroll:
+        const spotifyToken = session?.spotifyToken ?? 'missing-access-token';
+        const response = await fetch(`https://api.spotify.com/v1/me`, {
+          method: 'GET',
+          headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${spotifyToken}`,
+            'Content-Type': 'application/json',
+          }
+        });
+        const payload: unknown = await response.json();
+        setSomeState(payload);
+      }
+
+      void worker();
+    }
+  }, [session]);
+
+  if (loading || !session) {
+    return null;
+  }
+
+  return <div>Placeholder</div>;
+}
--- webapp/pages/api/auth/[...nextauth].ts
+++ webapp/pages/api/auth/[...nextauth].ts
@@ -3,21 +3,71 @@
  * the Open Software License version 3.0.
  */
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 import * as env from 'env-var';
 import NextAuth from 'next-auth';
-import Adapters from 'next-auth/adapters'
+import Adapters from 'next-auth/adapters';
 import Providers from 'next-auth/providers';
 
-const prisma = new PrismaClient()
+const prisma = new PrismaClient();
+
+// Reference: https://developer.spotify.com/documentation/general/guides/scopes/
+const SPOTIFY_SCOPES: string[] = [
+  // Library
+  'user-library-modify',
+  'user-library-read',
+
+  // Listening history
+  'user-read-recently-played',
+  'user-top-read',
+  'user-read-playback-position',
+
+  // Spotify Connect
+  'user-read-playback-state',
+  'user-modify-playback-state',
+  'user-read-currently-playing',
+
+  // Playlists
+  'playlist-modify-public',
+  'playlist-modify-private',
+  'playlist-read-private',
+  'playlist-read-collaborative',
+
+  // User
+  'user-read-email',
+  'user-read-private',
+];
 
 export default NextAuth({
   adapter: Adapters.Prisma.Adapter({ prisma }),
+  callbacks: {
+    async session(session, user) {
+      // console.log(`>> ${JSON.stringify(user)} <<`);
+      const userId = user.id as number;
+      const result = await prisma.account.findFirst({
+        select: { accessToken: true },
+        where: {
+          userId,
+          providerId: 'spotify'
+        }
+      });
+
+      if (result?.accessToken) {
+        return {
+          ...session,
+          spotifyToken: result.accessToken,
+        };
+      }
+
+      return session;
+    },
+  },
   debug: false,
   providers: [
     Providers.Spotify({
       clientId: env.get('SPOTIFY_CLIENT_ID').required().asString(),
       clientSecret: env.get('SPOTIFY_CLIENT_SECRET').required().asString(),
+      scope: SPOTIFY_SCOPES,
     }),
   ],
 });
