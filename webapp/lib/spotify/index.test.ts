cdf94f6256f02ffea1bb2701f6b758e136f89d84
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-03-17 09:04:23 -0400

    Refactor test data to be more modular

--- webapp/lib/spotify/index.test.ts
+++ webapp/lib/spotify/index.test.ts
@@ -4,6 +4,95 @@ import { getTrackIds, smokeTest } from './index';
 
 type WrapperList = Spotify.TrackWrapper[];
 
+const publicUser: Spotify.PublicUserObject = {
+  display_name: 'Alex',
+  external_urls: {
+    spotify: 'https://open.spotify.com/user/phil.gs',
+  },
+  href: 'https://api.spotify.com/v1/users/phil.gs',
+  id: 'phil.gs',
+  type: 'user',
+  uri: 'spotify:user:phil.gs',
+};
+
+const song1: Spotify.TrackWrapper = {
+  added_at: '2021-03-05T15:21:46Z',
+  added_by: publicUser,
+  is_local: false,
+  track: {
+    name: 'Song 1',
+    id: '7p2T6ivlXN6n79DBfg8Lrv',
+  },
+};
+
+const song2: Spotify.TrackWrapper = {
+  added_at: '2021-03-05T15:21:46Z',
+  added_by: publicUser,
+  is_local: false,
+  track: {
+    name: 'Song 2',
+    id: '1fxm2w6M69YuCWrVFPHoQB',
+  },
+};
+
+const song3: Spotify.TrackWrapper = {
+  added_at: '2021-03-05T15:21:46Z',
+  added_by: publicUser,
+  is_local: false,
+  track: {
+    name: 'Song Free',
+    id: '2MycpboRozeeoFNqWGrnak',
+  },
+};
+
+const local: Spotify.TrackWrapper = {
+  added_at: '2021-03-05T15:21:46Z',
+  track: {
+    album: {
+      album_type: null,
+      artists: [],
+      available_markets: [],
+      external_urls: {},
+      href: null,
+      id: null,
+      images: [],
+      name: '2003-07-11: Mansfield, MA, USA (disc 3)',
+      release_date: null,
+      release_date_precision: null,
+      type: 'album',
+      uri: null,
+    },
+    artists: [
+      {
+        external_urls: {},
+        href: null,
+        id: null,
+        name: 'Pearl Jam',
+        type: 'artist',
+        uri: null,
+      },
+    ],
+    available_markets: [],
+    disc_number: 0,
+    duration_ms: 231000,
+    explicit: false,
+    external_ids: {},
+    external_urls: {},
+    href: null,
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore -- allow `null` for `wrapper.track.id`
+    id: null,
+    is_local: true,
+    name: 'Fortunate Son',
+    popularity: 0,
+    preview_url: null,
+    track_number: 0,
+    type: 'track',
+    uri:
+      'spotify:local:Pearl+Jam:2003-07-11%3A+Mansfield%2C+MA%2C+USA+%28disc+3%29:Fortunate+Son:231',
+  },
+};
+
 describe('Smoke test', () => {
   it('correctly adds 2 and 2', () => {
     expect(smokeTest(2, 2)).toEqual(4);
@@ -12,114 +101,19 @@ describe('Smoke test', () => {
 
 describe('Function `getTrackIds`', () => {
   it('extracts the ID from remote tracks', () => {
-    const sampleData: WrapperList = [
-      {
-        added_at: '2021-03-05T15:21:46Z',
-        track: {
-          name: 'Song 1',
-          id: '7p2T6ivlXN6n79DBfg8Lrv',
-        },
-      },
-      {
-        added_at: '2021-03-05T15:21:46Z',
-        track: {
-          name: 'Song 2',
-          id: '1fxm2w6M69YuCWrVFPHoQB',
-        },
-      },
-      {
-        added_at: '2021-03-05T15:21:46Z',
-        track: {
-          name: 'Song Free',
-          id: '2MycpboRozeeoFNqWGrnak',
-        },
-      },
-    ];
-    const expectedOutput = [
-      '7p2T6ivlXN6n79DBfg8Lrv',
-      '1fxm2w6M69YuCWrVFPHoQB',
-      '2MycpboRozeeoFNqWGrnak',
-    ];
+    const sampleData: WrapperList = [song1, song2, song3];
+    const expectedOutput = [song1.track.id, song2.track.id, song3.track.id];
 
     expect(getTrackIds(sampleData)).toEqual(expectedOutput);
   });
 
   it('extracts the ID from local and remote tracks', () => {
-    const sampleData: WrapperList = [
-      {
-        added_at: '2021-03-05T15:21:46Z',
-        track: {
-          name: 'Song 1',
-          id: '7p2T6ivlXN6n79DBfg8Lrv',
-        },
-      },
-      {
-        added_at: '2021-03-05T15:21:46Z',
-        track: {
-          name: 'Song 2',
-          id: '1fxm2w6M69YuCWrVFPHoQB',
-        },
-      },
-      {
-        added_at: '2021-03-05T15:21:46Z',
-        track: {
-          name: 'Song Free',
-          id: '2MycpboRozeeoFNqWGrnak',
-        },
-      },
-      {
-        added_at: '2021-03-05T15:21:46Z',
-        track: {
-          album: {
-            album_type: null,
-            artists: [],
-            available_markets: [],
-            external_urls: {},
-            href: null,
-            id: null,
-            images: [],
-            name: '2003-07-11: Mansfield, MA, USA (disc 3)',
-            release_date: null,
-            release_date_precision: null,
-            type: 'album',
-            uri: null,
-          },
-          artists: [
-            {
-              external_urls: {},
-              href: null,
-              id: null,
-              name: 'Pearl Jam',
-              type: 'artist',
-              uri: null,
-            },
-          ],
-          available_markets: [],
-          disc_number: 0,
-          duration_ms: 231000,
-          explicit: false,
-          external_ids: {},
-          external_urls: {},
-          href: null,
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore -- allow `null` for `wrapper.track.id`
-          id: null,
-          is_local: true,
-          name: 'Fortunate Son',
-          popularity: 0,
-          preview_url: null,
-          track_number: 0,
-          type: 'track',
-          uri:
-            'spotify:local:Pearl+Jam:2003-07-11%3A+Mansfield%2C+MA%2C+USA+%28disc+3%29:Fortunate+Son:231',
-        },
-      },
-    ];
+    const sampleData: WrapperList = [song1, song2, song3, local];
     const expectedOutput = [
-      '7p2T6ivlXN6n79DBfg8Lrv',
-      '1fxm2w6M69YuCWrVFPHoQB',
-      '2MycpboRozeeoFNqWGrnak',
-      'spotify:local:Pearl+Jam:2003-07-11%3A+Mansfield%2C+MA%2C+USA+%28disc+3%29:Fortunate+Son:231',
+      song1.track.id,
+      song2.track.id,
+      song3.track.id,
+      local.track.uri,
     ];
 
     expect(getTrackIds(sampleData)).toEqual(expectedOutput);
