fcb849ef8c0328b5f2413725b034fd346e87dffc
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-03-21 11:45:32 -0400

    Add database migrations; increment patch version number

---
+++ webapp/prisma/migrations/20210321154105_migration_0_1_3/migration.sql
@@ -0,0 +1,142 @@
+-- CreateTable
+CREATE TABLE "Playlist" (
+    "id" SERIAL NOT NULL,
+    "spotifyId" TEXT NOT NULL,
+    "description" TEXT,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PlaylistSnapshot" (
+    "id" SERIAL NOT NULL,
+    "playlistId" INTEGER NOT NULL,
+    "description" TEXT,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "PlaylistTrack" (
+    "id" SERIAL NOT NULL,
+    "addedAt" TIMESTAMP(3) NOT NULL,
+    "position" INTEGER NOT NULL,
+    "trackId" INTEGER NOT NULL,
+    "snapshotId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Track" (
+    "id" SERIAL NOT NULL,
+    "spotifyId" TEXT NOT NULL,
+    "isLocal" BOOLEAN NOT NULL,
+    "name" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "accounts" (
+    "id" SERIAL NOT NULL,
+    "compound_id" TEXT NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "provider_type" TEXT NOT NULL,
+    "provider_id" TEXT NOT NULL,
+    "provider_account_id" TEXT NOT NULL,
+    "refresh_token" TEXT,
+    "access_token" TEXT,
+    "access_token_expires" TIMESTAMP(3),
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "sessions" (
+    "id" SERIAL NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+    "session_token" TEXT NOT NULL,
+    "access_token" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "users" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "email_verified" TIMESTAMP(3),
+    "image" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "verification_requests" (
+    "id" SERIAL NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires" TIMESTAMP(3) NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Playlist.spotifyId_unique" ON "Playlist"("spotifyId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Track.spotifyId_unique" ON "Track"("spotifyId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "accounts.compound_id_unique" ON "accounts"("compound_id");
+
+-- CreateIndex
+CREATE INDEX "providerAccountId" ON "accounts"("provider_account_id");
+
+-- CreateIndex
+CREATE INDEX "providerId" ON "accounts"("provider_id");
+
+-- CreateIndex
+CREATE INDEX "userId" ON "accounts"("user_id");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions.session_token_unique" ON "sessions"("session_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "sessions.access_token_unique" ON "sessions"("access_token");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users.email_unique" ON "users"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "verification_requests.token_unique" ON "verification_requests"("token");
+
+-- AddForeignKey
+ALTER TABLE "PlaylistSnapshot" ADD FOREIGN KEY ("playlistId") REFERENCES "Playlist"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlaylistTrack" ADD FOREIGN KEY ("trackId") REFERENCES "Track"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "PlaylistTrack" ADD FOREIGN KEY ("snapshotId") REFERENCES "PlaylistSnapshot"("id") ON DELETE CASCADE ON UPDATE CASCADE;
