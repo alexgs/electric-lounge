981ba87b63d392ccdfb18f3142d37bfa8d3bd537
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-03-26 17:48:19 -0400

    Get `pino` working with `tee` in a child process

--- webapp/scripts/capture-playlist-snapshot.ts
+++ webapp/scripts/capture-playlist-snapshot.ts
@@ -3,12 +3,32 @@
  * the Open Software License version 3.0.
  */
 
-import prisma from 'lib/prisma';
+import * as childProcess from 'child_process';
+import * as env from 'env-var';
+import pino from 'pino';
+import * as stream from 'stream';
 
+// This file needs to be compiled with `esModuleInterop`, like this:
+// npx tsc --esModuleInterop scripts/capture-playlist-snapshot.ts
+
+const LOG_PATH = env.get('LOG_PATH').default(process.cwd()).asString();
+
+// TODO Wire-up `tee` to capture console output, using the example from the `pino-tee` documentation
 async function main() {
+  const logThrough = new stream.PassThrough();
+  const log = pino({ name: 'capture-playlist-snapshot' }, logThrough) ;
+  const child = childProcess.spawn('tee', [`-a`, `log.txt`], {
+    cwd: process.cwd(),
+    env: process.env,
+  });
+  logThrough.pipe(child.stdin);
+
   const now = new Date();
-  console.log(`The current time is ${now.toISOString()}`);
-  return Promise.resolve({ exitCode: 0 });
+  log.info(`The current time is ${now.toISOString()}`);
+  child.kill('SIGTERM');
+  return new Promise((resolve) => {
+    setTimeout(() => resolve({ exitCode: 0 }), 1000);
+  });
 }
 
 main().catch((error) => {
