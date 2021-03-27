9ce83c8e8fd5fa2048c8f62804fe9fbeac0a24d1
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-03-27 12:56:40 -0400

    Switch to Winston for logging

--- webapp/scripts/capture-playlist-snapshot.ts
+++ webapp/scripts/capture-playlist-snapshot.ts
@@ -3,22 +3,31 @@
  * the Open Software License version 3.0.
  */
 
-import pino from 'pino';
-
-// This file needs to be compiled with `esModuleInterop`, like this:
-// npx tsc --esModuleInterop scripts/capture-playlist-snapshot.ts
+import winston from 'winston';
 
 const SCRIPT_NAME = 'capture-playlist-snapshot';
 
 async function main() {
-  const logger = pino({ name: SCRIPT_NAME });
+  const logger = winston.createLogger({
+    defaultMeta: { script: SCRIPT_NAME },
+    format: winston.format.combine(
+      winston.format.timestamp(),
+      winston.format.errors({ stack: true }),
+      winston.format.json()
+    ),
+    level: 'silly',
+    transports: [
+      new winston.transports.File({ filename: 'cron-trace.log' }),
+      new winston.transports.File({ filename: 'cron.log', level: 'info' }),
+    ]
+  });
 
-  logger.trace(`Script "${SCRIPT_NAME}" starting.`);
+  logger.verbose(`Script \`${SCRIPT_NAME}\` starting.`);
   const now = await new Promise<Date>(resolve => {
     resolve(new Date());
   });
   logger.info(`The current time is ${now.toISOString()}`);
-  logger.trace(`Script "${SCRIPT_NAME}" finished.`);
+  logger.verbose(`Script \`${SCRIPT_NAME}\` finished.`);
 }
 
 main().catch((error) => {
