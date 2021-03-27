acebfb90357b3464dddc2d275fcebd421a3f0790
Alex Gates-Shannon <alex.nebula99@gmail.com>
2021-03-27 13:22:54 -0400

    Format log messages

--- webapp/scripts/capture-playlist-snapshot.ts
+++ webapp/scripts/capture-playlist-snapshot.ts
@@ -7,26 +7,44 @@ import winston from 'winston';
 
 const SCRIPT_NAME = 'capture-playlist-snapshot';
 
+const myFormat = (info: Record<string, string>) =>
+  `[${info.timestamp}] ${info.script} ${info.level.toUpperCase()}: ` +
+  info.message;
+
 async function main() {
   const logger = winston.createLogger({
     defaultMeta: { script: SCRIPT_NAME },
     format: winston.format.combine(
       winston.format.timestamp(),
       winston.format.errors({ stack: true }),
-      winston.format.json()
+      winston.format.json(),
     ),
     level: 'silly',
     transports: [
       new winston.transports.File({ filename: 'cron-trace.log' }),
-      new winston.transports.File({ filename: 'cron.log', level: 'info' }),
-    ]
+      new winston.transports.File({
+        filename: 'cron.log',
+        level: 'info',
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.printf(myFormat),
+        ),
+      }),
+      new winston.transports.Console({
+        level: 'info',
+        format: winston.format.combine(
+          winston.format.timestamp(),
+          winston.format.printf(myFormat),
+        ),
+      }),
+    ],
   });
 
   logger.verbose(`Script \`${SCRIPT_NAME}\` starting.`);
-  const now = await new Promise<Date>(resolve => {
+  const now = await new Promise<Date>((resolve) => {
     resolve(new Date());
   });
-  logger.info(`The current time is ${now.toISOString()}`);
+  logger.info(`The current time is ${now.toISOString()}.`);
   logger.verbose(`Script \`${SCRIPT_NAME}\` finished.`);
 }
 
